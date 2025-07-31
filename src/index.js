3e505a011fa94d6da62e647beea42f842e2212f3
Erick Muke <muke7881@gmail.com>
2025-07-31 00:34:30 -0600

    feat(setup): Incializacion del ambiente de pruebas desde prisma

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express, { json } from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.listen(3000);
+console.log('Server running on port', 3000);
